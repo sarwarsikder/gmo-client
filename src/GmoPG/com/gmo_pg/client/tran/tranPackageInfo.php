165361217ba6429d2f19622a6d94bdb528d1f31f
Sarwar Sikder <sarwar.sikder@kaicomsol.com>
2022-01-12 13:50:23 +0600

    error message added

--- src/GmoPG/com/gmo_pg/client/tran/tranPackageInfo.php
+++ src/GmoPG/com/gmo_pg/client/tran/tranPackageInfo.php
@@ -1,7 +1,9 @@
 <?php
+
+namespace GmoPG\com\gmo_pg\client\tran;
 /**
  * パッケージ tran
- * 
+ *
  * <p>
  *  PGカード決済サービスとの通信を行うクラスです。実行する処理に対応したinputクラスのオブジェクトを
  * 受け取り、execメソッドで通信を行い、その戻りとして各outputクラスのオブジェクトが返されます。<br />
